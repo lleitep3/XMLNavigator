ad8dde11c7d2feb8361618837b91c98db7e68780
Leandro <lleitep3@gmail.com>
2012-04-21 19:25:04 -0300

    first commit lib

---
+++ xmlNavigator.php
@@ -0,0 +1,73 @@
+<?php
+
+class XMLNavigator {
+
+    private $xml = null;
+    private $name = null;
+    private $attr = array();
+
+    public function __construct($xml) {
+        try {
+            if ($xml instanceof SimpleXMLElement)
+                $this->xml = $xml;
+            elseif (is_file($xml))
+                $this->xml = simplexml_load_file($xml);
+            else
+                $this->xml = simplexml_load_string($xml);
+        } catch (Exception $e) {
+            // register error log
+            error_log($e->getMessage() . '-' . $e->getTraceAsString());
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    protected function __getNodes__($name, $attr = array()) {
+
+        $this->name = $name;
+        $this->attr = $attr;
+
+        $nodes = $this->__searchNode__();
+        $arr = array();
+
+        foreach ($nodes as $node)
+            $arr[] = new static($node);
+
+        return $arr;
+    }
+
+    protected function __searchNode__() {
+        $path = "//{$this->name}";
+
+        foreach ($this->attr as $k => $v)
+            $path .= "[@{$k}='{$v}']";
+        try {
+            $nodes = $this->xml->xpath($path);
+        } catch (Exception $e) {
+            throw new Exception("node {$this->name} not Found");
+        }
+        return $nodes;
+    }
+
+    public function __get($name) {
+        $nodes = $this->__getNodes__($name);
+        return (is_array($nodes) ? current($nodes) : $nodes);
+    }
+
+    public function all($name, $attr = array()) {
+        return $this->__getNodes__($name, $attr);
+    }
+
+    public function __toString() {
+        return $this->xml->asXML();
+    }
+
+    public function __call($name, $attr) {
+        $nodes = $this->__getNodes__($name, $attr[0]);
+        return (is_array($nodes) ? current($nodes) : $nodes);
+    }
+
+}
+
+$file = 'demo.xml';
+$xml = new XMLNavigator($file);
+var_dump($xml->teacher(array('nome' => 'Bruno'))->all('studant',array('idade'=>'18')));
